e9175514de64aa4ce3ba30ddb059b85047744c6a
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-14 15:02:41 +0300

    Brackets/feature: implemeted logic to chk simple brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrBr = ["(", ")"];
+  let countLoop = 0;
+  let result = "undef";
+  let pos = 0;
+
+  while (result === "undef") {
+      arrBr.forEach(function(item, i, arrBr) {
+          if ((str[pos] == item) && (i == 0)) countLoop++;
+          if ((str[pos] == item) && (i == 1)) {
+              if (countLoop > 0) countLoop--;
+              else result = "Err";
+          }
+      })
+      pos++;
+      if (pos>str.length) result = "finish";
+  };
+  console.log ("___________________________");
+  console.log("str= ", str);
+  console.log ("countloop= ",countLoop, "result= ", result);
+  if (result == "Err") {console.log ("= ", false); return false}
+
+  if (countLoop == 0) {console.log ("= ", true); return true}
+  else {console.log ("= ", false); return false}
+
 }
