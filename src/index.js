aa4f9fdf4bc81575a891f9f7b282945c467f1fc5
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-14 18:55:57 +0300

    Brackets/ return to start, need develop new algorithm but left some code from that algorithm

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  let arrBr = ["(", ")"];
-  let countLoop = 0;
-  let result = "undef";
-  let pos = 0;
+    let result = "undef";
 
-  while (result === "undef") {
-      arrBr.forEach(function(item, i, arrBr) {
-          if ((str[pos] == item) && (i == 0)) countLoop++;
-          if ((str[pos] == item) && (i == 1)) {
-              if (countLoop > 0) countLoop--;
-              else result = "Err";
-          }
-      })
-      pos++;
-      if (pos>str.length) result = "finish";
-  };
-  console.log ("___________________________");
-  console.log("str= ", str);
-  console.log ("countloop= ",countLoop, "result= ", result);
-  if (result == "Err") {console.log ("= ", false); return false}
+    while (result === "undef") {
+        pos++;
+        if (pos > str.length) result = "finish";
+    };
 
-  if (countLoop == 0) {console.log ("= ", true); return true}
-  else {console.log ("= ", false); return false}
+
+    console.log("___________________________");
+    console.log("str= ", str);
+
+    if (result == "Err") {
+        console.log("= ", false);
+        return false
+    }
 
 }
