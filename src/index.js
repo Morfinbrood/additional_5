808af885b07e25426b9ecc59dd0e44e4794b4d11
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-16 09:49:19 +0300

    Brackets/refactoring: also added chk input str with zero lenght

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 ﻿module.exports = function check(str, bracketsConfig) {
-    if (str.length % 2 !== 0) return false;
+    if ((str.length % 2 !== 0) || (str.length == 0)) return false;
     // функция находит тип скобок по открывающей скобке и возвращает символ закрывающей скобки
     function findTypeClosBracket(openBracket, bracketsConfig) {
         let result = "undef";
-        bracketsConfig.forEach(function(element, i, bracketsConfig) {
+        bracketsConfig.forEach(function(element) {
             if (openBracket == element[0]) result = element[1]
         });
         return result;
@@ -16,7 +16,8 @@
     for (let i = 0; i < length - 1; i++) {
         if (str[i + 1] == findTypeClosBracket(str[i], bracketsConfig)) {
             str = delSubString(str, i, i + 1);
-            i = -1; // -1 потому что после i++ оно стартует с i=1
+            // i=-1 потому что после i++ оно стартует с i=1
+            i = -1;
             length = length - 2;
         }
     }
